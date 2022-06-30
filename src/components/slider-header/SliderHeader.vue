daad12e4142c1f68e3a6ee0eed5698a8515af06d
locbp <locbq@vmodev.com>
2022-06-30 16:55:32 +0700

    update

--- src/components/slider-header/SliderHeader.vue
+++ src/components/slider-header/SliderHeader.vue
@@ -1,22 +1,29 @@
 <template>
-  <Carousel :itemsToShow="7" :snapAlign="right">
+  <div class="navigation flex">
+    <Carousel
+      :itemsToShow="9"
+      :itemsToScroll="8"
+      :snapAlign="right"
+      ref="myCarousel"
+    >
       <Slide v-for="(image, index) in images" :key="index">
-      <div class="flex items-center">
-        <div class="w-14 h-14">
-          <img :src="image.url" />
+        <div class="flex items-center">
+          <div class="w-14 h-14">
+            <img :src="image.url" />
+          </div>
+          <span>{{ image.name }}</span>
         </div>
-        <span>{{image.name}}</span>
-      </div>
-       
       </Slide>
-    <template #addons>
-      <Navigation />
-    </template>
-  </Carousel>
+    </Carousel>
+    <div class="flex-1 flex space-x-2 pl-6 py-[6px]">
+      <button @click="myCarousel.prev()">Prev</button>
+      <button @click="myCarousel.next()">next</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, onMounted,toRefs } from "vue";
+import { ref, onMounted, toRefs } from "vue";
 import { Carousel, Navigation, Slide } from "vue3-carousel";
 
 import "vue3-carousel/dist/carousel.css";
@@ -29,14 +36,40 @@ export default {
   components: {
     Carousel,
     Slide,
-    Navigation,
+  },
+  setup() {
+    const myCarousel = ref(null);
+
+    return {
+      myCarousel,
+    };
   },
 };
 </script>
 
-<style>
-.carousel__prev{
+<style lang="css">
+.navigation .carousel {
+  width: 100%;
+  overflow: hidden;
+  cursor: grab;
+}
+
+.navigation .carousel__prev {
   right: 0;
   left: unset;
+}
+
+.navigation .carousel__slide {
+  justify-content: flex-start;
+  width: unset !important;
+  padding-left: 0.25rem;
+  padding-right: 0.75rem;
+  border-radius: 0.5rem;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.carousel__prev--in-active,
+.carousel__next--in-active {
+  display: none;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,5 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./index.css";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
 
-library.add(fas);
-
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(Antd)
-  .component("fa", FontAwesomeIcon)
-  .mount("#app");
+createApp(App).use(store).use(router).use(Antd).mount("#app");
